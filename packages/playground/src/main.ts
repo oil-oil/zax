9671360853e5884f4cabdb8d5648700370aad067
oiloil <2228586315@qq.com>
2023-10-24 23:59:12 +0800

    feat: playgroud add import map

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import Zax from "zax"
 import "zax/style.css"
 
-createApp(App).use(Zax).mount('#app')
+const app = createApp(App).use(Zax)
+
+app.mount('#app')
